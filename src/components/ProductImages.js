707169953364a4e651e00b3ecf8e559f385d83f6
ruslanepi <ruslanep2@gmail.com>
2021-10-26 22:10:42 +0300

    created full single product

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,8 +1,28 @@
 import React, { useState } from 'react'
+import { FaMapPin } from 'react-icons/fa'
 import styled from 'styled-components'
 
-const ProductImages = () => {
-  return <h4>product images</h4>
+const ProductImages = ({ images = [{ url: '' }] }) => {
+  const [activeImage, setActiveImage] = useState(images[0])
+
+  return (
+    <Wrapper>
+      <img src={activeImage.url} alt='main image' className='main' />
+      <div className='gallery'>
+        {images.map((image, index) => {
+          return (
+            <img
+              key={index}
+              src={image.url}
+              alt={image.filename}
+              onClick={() => setActiveImage(image)}
+              className={`${image.url === activeImage.url ? 'active' : null}`}
+            />
+          )
+        })}
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -26,7 +26,7 @@ const products_reducer = (state, action) => {
     const featured_products = action.payload.filter(
       (product) => product.featured === true
     )
-    console.log(featured_products)
+
     return {
       ...state,
       products_loading: false,
@@ -37,6 +37,32 @@ const products_reducer = (state, action) => {
 
   if (action.type === GET_PRODUCTS_ERROR) {
     return { ...state, products_loading: false, products_error: true }
+  }
+
+  //single product
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    }
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: false,
+      single_product: action.payload,
+    }
+  }
+
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    }
   }
 
   return state
