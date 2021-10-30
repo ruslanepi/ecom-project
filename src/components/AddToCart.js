96e4d7cb22b64dd54adcecbc5e97b7659a8ffb14
ruslanepi <ruslanep2@gmail.com>
2021-10-30 15:07:55 +0300

    add to cart functionality

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext()
   const { id, stock, colors } = product
 
   const [activeColor, setActiveColor] = useState(colors[0])
@@ -57,7 +58,11 @@ const AddToCart = ({ product }) => {
           increase={increase}
           decrease={decrease}
         />
-        <Link to='/cart' className='btn'>
+        <Link
+          to='/cart'
+          className='btn'
+          onClick={() => addToCart(id, activeColor, amount, product)}
+        >
           add to cart
         </Link>
       </div>
